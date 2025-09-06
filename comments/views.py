5ae285059d70bd5bb336da7752f356bb52a1a76b
Anton Konyk <antonkonyk.com>
2025-09-06 20:38:36 +0200

    create CommentListView

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Comment
+from .serializers import (
+    CommentListSerializer,
+)
 
-# Create your views here.
+
+class CommentListView(generics.ListAPIView):
+    """
+    GET /api/comments/
+    Returns a list of root comments with the number of replies.
+    """
+    queryset = Comment.objects.filter(parent=None) \
+        .select_related("user") \
+        .prefetch_related("replies")
+    serializer_class = CommentListSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
