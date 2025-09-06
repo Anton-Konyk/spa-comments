a04dcb9f92fb404c2c4b23c21f99bceca384be78
Anton Konyk <antonkonyk.com>
2025-09-06 13:10:27 +0200

    create class Comment

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(
+        SpaUser, on_delete=models.CASCADE, related_name="comments"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    home_page = models.URLField(blank=True, null=True)
+    text = models.TextField()
+    file = models.FileField(
+        upload_to=comments_file_path,
+        blank=True,
+        null=True,
+        validators=[
+            FileExtensionValidator(
+                allowed_extensions=["jpg", "jpeg", "png", "gif", "txt"]
+            ),
+            validate_file_size,
+        ],
+    )
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="replies",
+    )
+
