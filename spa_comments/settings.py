8f6549943f8768a37c5cb6cc5b36e5295d6f9ecd
Anton Konyk <antonkonyk.com>
2025-09-06 21:07:25 +0200

    add throttling

--- spa_comments/settings.py
+++ spa_comments/settings.py
@@ -107,8 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
 SPECTACULAR_SETTINGS = {
     "TITLE": "SPA Comments API",
     "DESCRIPTION": "Documentation for SPA Comments API.",
