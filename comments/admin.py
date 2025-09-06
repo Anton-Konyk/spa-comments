c7eb9ee428a42904015f1a87a25008962958f11b
Anton Konyk <antonkonyk.com>
2025-09-06 13:49:34 +0200

    register Comment for admin

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from comments.models import Comment
+
+
+from django.contrib import admin
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "short_text", "created_at", "is_reply")
+    list_filter = ("created_at",)
+    search_fields = ("text", "user__username")
+    ordering = ("-created_at",)
+    raw_id_fields = ("user", "parent")
+
+    def short_text(self, obj):
+        return obj.text[:25].replace("\n", " ")
+    short_text.short_description = "Text"
+
+    def is_reply(self, obj):
+        return obj.is_reply
+    is_reply.boolean = True
+    is_reply.short_description = "Reply?"
