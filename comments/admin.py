aafcadccb8959f3a826c90ab9c4cd508d153217a
Anton Konyk <antonkonyk.com>
2025-09-07 20:03:54 +0200

    add parent_preview, and new search_fields parent__text, parent__user__username

--- comments/admin.py
+++ comments/admin.py
@@ -9,9 +9,9 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ("user", "short_text", "created_at", "is_reply")
+    list_display = ("user", "short_text", "created_at", "is_reply", "parent_preview")
     list_filter = ("created_at",)
-    search_fields = ("text", "user__username")
+    search_fields = ("text", "user__username", "parent__text", "parent__user__username")
     ordering = ("-created_at",)
     raw_id_fields = ("user", "parent")
 
@@ -23,3 +23,10 @@ class CommentAdmin(admin.ModelAdmin):
         return obj.is_reply
     is_reply.boolean = True
     is_reply.short_description = "Reply?"
+
+    def parent_preview(self, obj):
+        if obj.parent:
+            parent_text = obj.parent.text[:25].replace("\n", " ")
+            return f"{obj.parent.user.username}: {parent_text}"
+        return "-"
+    parent_preview.short_description = "In reply to"
