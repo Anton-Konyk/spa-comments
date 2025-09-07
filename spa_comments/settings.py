a0fd708eb7b5544deb59baa7f51d039d40103457
Anton Konyk <antonkonyk.com>
2025-09-07 20:11:07 +0200

    create pagination settings

--- spa_comments/settings.py
+++ spa_comments/settings.py
@@ -113,6 +113,11 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
+    "DEFAULT_FILTER_BACKENDS": [
+        "rest_framework.filters.OrderingFilter",
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
