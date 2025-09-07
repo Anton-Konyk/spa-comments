77ea80e10d2e33aeb93696416511117ab0a86cab
Anton Konyk <antonkonyk.com>
2025-09-07 20:06:09 +0200

    add avatar fild to admin

--- users/admin.py
+++ users/admin.py
@@ -6,7 +6,14 @@ from users.models import SpaUser
 
 @admin.register(SpaUser)
 class SpaUserAdmin(UserAdmin):
-    list_display = ("username", "email", "is_staff", "is_active")
+    list_display = ("username", "email", "is_staff", "is_active", "avatar")
     list_filter = ("is_staff", "is_superuser", "is_active", "groups")
     search_fields = ("username", "email")
     ordering = ("username",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("avatar",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("avatar",)}),
+    )
