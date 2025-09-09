3f6ce04b6b7b476d45d5318858054afaded044c8
Anton Konyk <antonkonyk.com>
2025-09-09 20:16:12 +0200

    create config.urls path, add v1 to paths

--- spa_comments/urls.py
+++ spa_comments/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    path('api/', include('comments.urls')),
+    path('api/v1/', include('comments.urls')),
+    path('api/v1/', include('config.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
