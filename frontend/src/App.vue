e7aa56dce428839fe1c579ddb7e33ec8f494c4ee
Anton Konyk <antonkonyk.com>
2025-09-09 16:11:23 +0200

    create main code for app.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>SPA Comments</h1>
+    <CommentList />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CommentList from './components/CommentList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { CommentList }
+})
+</script>
