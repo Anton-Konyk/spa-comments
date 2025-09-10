2110268af8ee0add4effa3ac51d19fb80e2cc2e4
Anton Konyk <antonkonyk.com>
2025-09-10 17:13:12 +0200

    deactivate DEFAULT_THROTTLE_RATES settings

--- spa_comments/settings.py
+++ spa_comments/settings.py
@@ -123,7 +123,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.AnonRateThrottle",
         "rest_framework.throttling.UserRateThrottle",
     ],
-    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
+    # "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": PAGE_SIZE,
