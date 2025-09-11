02432f97dcb512d5639de99ed4c40c9b40d098e4
Anton Konyk <antonkonyk.com>
2025-09-11 21:10:46 +0200

    create LoginSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+
 from .models import SpaUser
 
 
@@ -18,3 +20,28 @@ class SpaUserSerializer(serializers.ModelSerializer):
                 return request.build_absolute_uri(obj.avatar.url)
             return obj.avatar.url
         return None
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username_or_email = data.get("username")
+        password = data.get("password")
+
+        user = authenticate(username=username_or_email, password=password)
+        if not user:
+            try:
+                user_obj = SpaUser.objects.get(email=username_or_email.lower())
+                user = authenticate(username=user_obj.username, password=password)
+            except SpaUser.DoesNotExist:
+                pass
+
+        if not user:
+            raise serializers.ValidationError(
+                {"detail": "Invalid username/email or password."}
+            )
+
+        data["user"] = user
+        return data
