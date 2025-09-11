8da98686633100af6c918dfc55c47fd09e81ee3a
Anton Konyk <antonkonyk.com>
2025-09-11 21:08:21 +0200

    add mimetypes

--- spa_comments/settings.py
+++ spa_comments/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import mimetypes
 import os
 import corsheaders
 from pathlib import Path
@@ -170,3 +170,5 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+mimetypes.add_type("image/webp", ".webp", True)
