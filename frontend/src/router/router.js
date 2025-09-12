e4ae70a5b8117c7ab862691addca48081a81598d
Anton Konyk <antonkonyk.com>
2025-09-12 17:33:56 +0200

    add paths for Login.vue and Register.vue

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,11 +1,14 @@
-// src/router.js
 import { createRouter, createWebHistory } from 'vue-router'
 import CommentList from '../components/CommentList.vue'
 import CommentDetail from '../components/CommentDetail.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
 
 const routes = [
   { path: '/', name: 'CommentList', component: CommentList },
   { path: '/comment/:id', name: 'CommentDetail', component: CommentDetail, props: true },
+  { path: '/login', name: 'Login', component: Login },
+  { path: '/register', name: 'Register', component: Register },
 ]
 
 const router = createRouter({
