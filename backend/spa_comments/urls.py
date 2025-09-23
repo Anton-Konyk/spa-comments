c32a5b6a46177018ccb74291007cf440a7a7daa4
Anton Konyk <antonkonyk.com>
2025-09-23 14:57:06 +0200

    change settings for static

--- backend/spa_comments/urls.py
+++ backend/spa_comments/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.views.static import serve as media_serve
 
 from django.conf import settings
 
@@ -33,4 +34,6 @@ urlpatterns = [
     path('api/v1/', include('comments.urls')),
     path('api/v1/', include('config.urls')),
     path("api/v1/users/", include("users.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [
+    re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
+]
