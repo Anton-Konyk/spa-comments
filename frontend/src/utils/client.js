42c92fb46ae89e1e2e13b0dd93579b078ac3496a
Anton Konyk <antonkonyk.com>
2025-09-23 17:14:06 +0200

    change logic

--- frontend/src/utils/client.js
+++ frontend/src/utils/client.js
@@ -1,38 +1,30 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
-// import { ensureCsrf } from './csrf.js';
-
-// const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-// const SAFE = new Set(['get', 'head', 'options', 'trace']);
-//
-// const client = axios.create({
-//   baseURL: BASE_URL,
-//   withCredentials: true,
-// });
-//
-// client.interceptors.request.use(async (config) => {
-//   const method = (config.method || 'get').toLowerCase();
-//   if (!SAFE.has(method)) {
-//     const token = await ensureCsrf();
-//     if (token) {
-//       config.headers = config.headers || {};
-//       config.headers['X-CSRFToken'] = token;
-//       config.headers['X-Requested-With'] = 'XMLHttpRequest';
-//     }
-//   }
-//   return config;
-// });
-
-// export default client;
+import { getCachedCsrfToken, ensureCsrf } from '@/utils/csrf.js';
 
 const client = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
-client.interceptors.request.use((config) => {
-  const t = Cookies.get('csrftoken');
-  if (t) config.headers['X-CSRFToken'] = t;
+client.interceptors.request.use(async (config) => {
+  const method = (config.method || 'get').toLowerCase();
+  const needsCsrf = ['post', 'put', 'patch', 'delete'].includes(method);
+
+  if (needsCsrf) {
+    let token = getCachedCsrfToken();
+    if (!token) token = await ensureCsrf();
+
+    if (token) {
+      config.headers = config.headers || {};
+      if (!('X-CSRFToken' in config.headers)) {
+        config.headers['X-CSRFToken'] = token;
+      }
+      if (!('X-Requested-With' in config.headers)) {
+        config.headers['X-Requested-With'] = 'XMLHttpRequest';
+      }
+    }
+  }
+
   return config;
 });
 
