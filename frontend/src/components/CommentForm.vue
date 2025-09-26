ae60e26adbc5834f26f6a29cf958459861c2447e
Anton Konyk <antonkonyk.com>
2025-09-26 19:43:49 +0200

    add view new comment

--- frontend/src/components/CommentForm.vue
+++ frontend/src/components/CommentForm.vue
@@ -38,6 +38,22 @@
 
       <input type="file" accept=".jpg,.jpeg,.png,.gif,.txt" @change="handleFileChange" />
 
+      <div class="form-actions">
+        <button type="button" @click="showDraftPreview = true">Preview</button>
+        <button type="submit" :disabled="submitting">
+          {{ submitting ? 'Submitting...' : 'Submit' }}
+        </button>
+        <button type="button" class="cancel-btn" @click="cancelReply">Cancel</button>
+      </div>
+
+      <!-- Draft preview modal -->
+      <div v-if="showDraftPreview" class="txt-modal">
+        <div class="txt-content">
+          <button class="txt-close" @click="showDraftPreview = false">×</button>
+          <div v-html="draftPreviewHtml"></div>
+        </div>
+      </div>
+
       <!-- reCAPTCHA v2 -->
       <div ref="captcha" class="g-recaptcha"></div>
 
@@ -124,6 +140,9 @@ const SANITIZE_OPTS = {
   // Accept only http(s) or mailto links
   ALLOWED_URI_REGEXP: /^(?:https?:|mailto:)/i,
 };
+
+const showDraftPreview = ref(false);
+const draftPreviewHtml = computed(() => DOMPurify.sanitize(text.value.trim(), SANITIZE_OPTS));
 
 /**
  * Stack-based check to ensure:
