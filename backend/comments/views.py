3523049f77c64e9d80738d87f4ea75ffe0ab89ab
Anton Konyk <antonkonyk.com>
2025-09-29 11:05:58 +0200

    add supports sorting OrderingFilter for username, email, created_at

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Comment
 from .serializers import (
@@ -12,12 +13,17 @@ class CommentListView(generics.ListAPIView):
     """
     GET /api/comments/
     Returns a list of root comments with the number of replies.
+    Supports sorting: user__username, user__email, created_at
     """
     queryset = Comment.objects.filter(parent=None) \
         .select_related("user") \
         .prefetch_related("replies")
     serializer_class = CommentListSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["user__username", "user__email", "created_at"]
+    ordering = ["-created_at"]
 
 
 class CommentDetailView(generics.RetrieveAPIView):
